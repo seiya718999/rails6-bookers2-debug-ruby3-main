2c7428593540493c5f1cc6cabe0a42650e93df57
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-08-02 01:28:22 +0000

    [interruption]ouyou2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @abook = Book.new
@@ -44,5 +45,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
