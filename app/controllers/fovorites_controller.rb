7e85e641ebf384578101f157d0209af1596210c7
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-08-02 06:59:21 +0000

    [interruption]ouyou3

---
+++ app/controllers/fovorites_controller.rb
@@ -0,0 +1,17 @@
+class FovoritesController < ApplicationController
+  
+  def create
+    book = Book.find(params[book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_back fallback_location: books_path
+  end
+  
+  def destroy
+    book = Book.find(params[book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.destroy
+    redirect_back fallback_location: books_path
+  end
+  
+end
